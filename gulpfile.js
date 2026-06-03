7dbf9ee81e8891c27e3a2a98bde71a6111303f8a
Imanov <e.imanov@solarl.ru>
2026-06-03 12:39:08 +0300

    переработка gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,8 @@ let path = {
     css: source_folder + "/scss/*.scss",
     js: source_folder + "/js/*.js",
     documents: source_folder + "/documents/**/*",
-    img: source_folder + "/img/**",
-    fonts: source_folder + "/fonts/*.woff2",
+    img: source_folder + "/img/**/*",
+    fonts: source_folder + "/fonts/*.*",
     sitemap: source_folder + "/sitemap.xml",
     robots: source_folder + "/robots.txt",
   },
@@ -22,111 +22,121 @@ let path = {
     sitemap: project_folder + "/",
     robots: project_folder + "/",
   },
-
   watch: {
     html: source_folder + "/**/*.html",
     css: source_folder + "/**/*.scss",
     js: source_folder + "/**/*.js",
-    img: source_folder + "/img/",
+    img: source_folder + "/img/**/*",
   },
-  clean: "./" + project_folder + "/",
+  clean: "./" + project_folder
 };
 
-const gulp = require("gulp"),
-  htmlmin = require("gulp-htmlmin"),
-  browserSyncApp = require("browser-sync"),
-  concat = require("gulp-concat"),
-  uglify = require("gulp-uglify"),
-  del = require("del"),
-  autoprefixer = require("gulp-autoprefixer"),
-  scss = require('gulp-sass')(require('sass'));
-let rename = require("gulp-rename");
-let cleanCSS = require("gulp-clean-css");
+const gulp = require("gulp");
+const htmlmin = require("gulp-htmlmin");
+const browserSync = require("browser-sync").create();
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const del = require("del");
+const autoprefixer = require("gulp-autoprefixer");
+const scss = require("gulp-sass")(require("sass"));
+const rename = require("gulp-rename");
+const cleanCSS = require("gulp-clean-css");
 
+// Оптимизация HTML
 function html() {
   return gulp
     .src(path.src.html)
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest(project_folder))
-    .pipe(browserSyncApp.stream());
+    .pipe(browserSync.stream());
 }
 
+// Копирование документов
 function documents() {
   return gulp
     .src(path.src.documents)
-    .pipe(gulp.dest(path.build.documents))
-    .pipe(browserSyncApp.stream());
+    .pipe(gulp.dest(path.build.documents));
 }
 
+// Копирование шрифтов
 function fonts() {
-  return gulp.src(path.src.fonts).pipe(gulp.dest(path.build.fonts));
+  return gulp
+    .src(path.src.fonts)
+    .pipe(gulp.dest(path.build.fonts));
 }
 
+// Копирование sitemap
 function sitemap() {
   return gulp
     .src(path.src.sitemap)
-    .pipe(gulp.dest(path.build.sitemap))
-    .pipe(browserSyncApp.stream());
+    .pipe(gulp.dest(path.build.sitemap));
 }
 
+// Копирование robots.txt
 function robots() {
   return gulp
     .src(path.src.robots)
-    .pipe(gulp.dest(path.build.robots))
-    .pipe(browserSyncApp.stream());
+    .pipe(gulp.dest(path.build.robots));
 }
 
+// Компиляция и оптимизация CSS
 function css() {
   return gulp
     .src(path.src.css)
     .pipe(scss({ outputStyle: 'expanded' }))
     .pipe(autoprefixer(['last 15 versions']))
-    .pipe(cleanCSS())
-    .pipe(rename({ suffix: '.min', prefix: '' }))
     .pipe(concat("style.min.css"))
+    .pipe(cleanCSS())
     .pipe(gulp.dest(path.build.css))
-    .pipe(browserSyncApp.stream());
+    .pipe(browserSync.stream());
 }
 
+// Минификация JS
 function js() {
   return gulp
     .src(path.src.js)
     .pipe(uglify())
     .pipe(concat("scripts.min.js"))
-    .pipe(gulp.dest(path.build.js));
+    .pipe(gulp.dest(path.build.js))
+    .pipe(browserSync.stream());
 }
 
-gulp.task("browserSyncApp", function () {
-  browserSyncApp.init({
-    server: {
-      baseDir: "./" + project_folder + "/",
-    },
-    port: '4200',
-    notify: false,
-  });
-});
-
-async function imageminApp() {
-  gulp
+// Копирование изображений
+function imageminApp() {
+  return gulp
     .src(path.src.img)
     .pipe(gulp.dest(path.build.img))
-    .pipe(browserSyncApp.stream());
+    .pipe(browserSync.stream());
+}
+// Очистка папки сборки
+function clean() {
+  return del(path.clean);
 }
 
-gulp.task("removedist", function () {
-  return del("dist");
-});
+// Запуск сервера и автообновление
+function browserSyncServe() {
+  browserSync.init({
+    server: {
+      baseDir: "./" + project_folder
+    },
+    port: 4200,
+    notify: false
+  });
+}
 
+// Наблюдение за изменениями
 function watchFiles() {
-  gulp.watch([path.watch.html], html);
-  gulp.watch([path.watch.css], css);
-  gulp.watch([path.watch.js], js);
-  gulp.watch([path.watch.img], imageminApp);
+  gulp.watch(path.watch.html, html);
+  gulp.watch(path.watch.css, css);
+  gulp.watch(path.watch.js, js);
+  gulp.watch(path.watch.img, imageminApp);
 }
 
-const build = gulp.series("removedist", html, js, css, imageminApp, documents, fonts, sitemap, robots);
+// Сборка проекта
+const build = gulp.series(clean, html, js, css, imageminApp, documents, fonts, sitemap, robots);
 
-const watch = gulp.parallel(build, watchFiles, "browserSyncApp");
+// Разработка с автообновлением
+const watch = gulp.parallel(build, watchFiles, browserSyncServe);
 
 exports.html = html;
 exports.documents = documents;
@@ -136,5 +146,6 @@ exports.imageminApp = imageminApp;
 exports.fonts = fonts;
 exports.sitemap = sitemap;
 exports.robots = robots;
-exports.watch = watch;
+exports.clean = clean;
 exports.build = build;
+exports.watch = watch;
